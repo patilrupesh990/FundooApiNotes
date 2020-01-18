39c822fe6b63ae950705aa1de9f10e3189930eba
Rupesh <patilrupesh990@gmail.com>
2020-01-18 19:31:14 +0530

    [Rupeshpatil].updated HibernateUtil

---
+++ src/main/java/com/bridgelabz/notes/util/HibernateUtil.java
@@ -0,0 +1,57 @@
+package com.bridgelabz.notes.util;
+
+import java.io.Serializable;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.bridgelabz.notes.model.Note;
+
+@Component
+@SuppressWarnings("unchecked")
+public class HibernateUtil<T> 
+{
+	@Autowired 
+	private EntityManager entityManager;
+	
+	
+	public T save(T object)
+	{
+		Session session = entityManager.unwrap(Session.class);
+		return (T) session.save(object);
+	}
+	
+	public Query<T> createQuery(String query)
+	{
+		Session session = entityManager.unwrap(Session.class);
+		return session.createQuery(query);
+	}
+	
+	public void update(T object)
+	{
+		Session session = entityManager.unwrap(Session.class);
+		 session.update(object);
+	}
+	public T updateAndGetObject(String query)
+	{
+		Session session = entityManager.unwrap(Session.class);
+		return (T) session.createQuery(query).list().stream().findFirst();
+	}
+	
+	public Query<T> select(String query)
+	{
+		Session session = entityManager.unwrap(Session.class);
+		return session.createQuery(query);
+	}
+	public T getCurrentNote(Serializable value)
+	{
+		Session session = entityManager.unwrap(Session.class);
+		return (T) session.get(Note.class,value);
+	}
+//	Note noteObj = currentSession.get(Note.class, noteId);
+
+}
