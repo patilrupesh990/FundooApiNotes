f5d503ad3768984f2c8a186c6d51f401acc7ab35
Rupesh <patilrupesh990@gmail.com>
2020-01-18 19:25:54 +0530

    [Rupeshpatil].updated NotesDaoimpl

--- src/main/java/com/bridgelabz/notes/dao/NotesDaoimpl.java
+++ src/main/java/com/bridgelabz/notes/dao/NotesDaoimpl.java
@@ -2,7 +2,7 @@ package com.bridgelabz.notes.dao;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
+import javax.transaction.Transaction;
 
 import org.hibernate.Session;
 import org.hibernate.query.Query;
@@ -10,96 +10,99 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.bridgelabz.notes.model.Note;
+import com.bridgelabz.notes.util.HibernateUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
-
 @Repository
 @Slf4j
-public class NotesDaoimpl implements INotesDao{
-	
-	@Autowired 
-	private EntityManager entityManager;
-	
+public class NotesDaoimpl implements INotesDao {
+
+	@Autowired
+	HibernateUtil<Note> hibernateUtil;
+
 	@Override
 	public Note createNote(Long userId, Note note) {
-		Session session = entityManager.unwrap(Session.class);
 		note.setUserId(userId);
-		session.save(note);
+		hibernateUtil.save(note);
 		log.info("New Note Created..");
 		return note;
 	}
-	
+
 	@Override
 	public Note getNoteById(Long noteId) {
-		Session currentSession = entityManager.unwrap(Session.class);
-		return currentSession.get(Note.class, noteId);
+
+		String query = "FROM Note WHERE id =:noteId";
+		Query<Note> hquery = hibernateUtil.select(query);
+		hquery.setParameter("noteId", noteId);
+		return hquery.getSingleResult();
 	}
 
 	@Override
 	public Note updateNote(Long noteId, Note note, Long userId) {
-		Session currentSession = entityManager.unwrap(Session.class);
-		Note noteObj = currentSession.get(Note.class, noteId);
-		if (noteObj != null) {
 			note.setUserId(userId);
-			currentSession.update(note);
+			hibernateUtil.update(note);
 			return note;
-		}
-		return noteObj;
 	}
 
 	@Override
 	public Integer deleteNote(Long userId, Long noteId) {
-		Session session = entityManager.unwrap(Session.class);
-		@SuppressWarnings("unchecked")
-		Query<Note> query=session.createQuery("DELETE FROM Note WHERE id = :noteid AND userId =:userid");
-		query.setParameter("noteid", noteId);
-		query.setParameter("userid", userId);
-		return query.executeUpdate();
+
+		String query = "DELETE FROM Note WHERE id = :noteid AND userId =:userid";
+		Query<Note> hQuery = hibernateUtil.createQuery(query);
+		hQuery.setParameter("noteid", noteId);
+		hQuery.setParameter("userid", userId);
+		return hQuery.executeUpdate();
 	}
-	
+
 	@Override
 	public List<Note> getAllNoteList(Long userId) {
-		Session session=entityManager.unwrap(Session.class);
-		@SuppressWarnings("unchecked")
-		Query<Note> query=session.createQuery("from Note where userId=:userid");
-		query.setParameter("userid", userId);
-		
-		return query.list();
+		String query = "FROM Note WHERE userId =:userid";
+		Query<Note> hquery = hibernateUtil.createQuery(query);
+		hquery.setParameter("userid", userId);
+		return hquery.list();
 	}
-	
-	
 
 	@Override
-	public List<Note> getNoteList(Integer uId, String noteCategory) {
-		// TODO Auto-generated method stub
+	public Integer pinUnpinNote(Long userId, Long noteId) {
+		Note noteObject = hibernateUtil.getCurrentNote(noteId);
+		if (noteObject != null) {
+			noteObject.setPinned(!noteObject.isPinned());
+			hibernateUtil.update(noteObject);
+			return 1;
+		}
+		return 0;
+	}
+
+	public Integer isTrash(Long noteId) {
+		Note noteObject = hibernateUtil.getCurrentNote(noteId);
+		if (noteObject != null) {
+			noteObject.setTrashed(!noteObject.isTrashed());
+			hibernateUtil.update(noteObject);
+			return 1;
+		}
+		return 0;
+	}
+	
+	public List<Note> getNoteList(Integer userId, String noteCategory) {
+			
 		return null;
 	}
-
-	
-
 	@Override
-	public void moveToTrash(Integer nId) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<Note> getTrashedNoteList(Integer uId) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Note> getTrashedNoteList(Long userId) {
+		String query = "FROM Note WHERE isTrashed is true AND userId=:userid";
+		Query<Note> hquery = hibernateUtil.select(query);
+		hquery.setParameter("userid", userId);
+		return hquery.list();
 	}
 
 	@Override
 	public void archiveNote(Note note) {
-		// TODO Auto-generated method stub
-		
+
 	}
-	
-	public  Session getSession()
-	{
-		return entityManager.unwrap(Session.class);
+
+	public Session getSession() {
+		return null;
 	}
-	
 
 }
