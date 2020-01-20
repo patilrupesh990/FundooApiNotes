0eea9c75ba312ba2dd1c03e4190920206408e343
Rupesh <patilrupesh990@gmail.com>
2020-01-20 19:23:00 +0530

    [Rupeshpatil].updated SwaggerConfig updated

--- src/main/java/com/bridgelabz/notes/configuration/SwaggerConfig.java
+++ src/main/java/com/bridgelabz/notes/configuration/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.notes.controller")).paths(regex("/note.*"))
+				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.notes.controller")).paths(regex("/.*"))
 				.build();
 	}
 }
