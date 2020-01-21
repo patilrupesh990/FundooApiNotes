a5cd95eea497c7e3d682d1541889d9af741c3192
Rupesh <patilrupesh990@gmail.com>
2020-01-21 18:54:44 +0530

    [Rupeshpatil].updated ILabelDao

--- src/main/java/com/bridgelabz/notes/dao/ILabelDao.java
+++ src/main/java/com/bridgelabz/notes/dao/ILabelDao.java
@@ -1,10 +1,17 @@
 package com.bridgelabz.notes.dao;
 
+import java.util.List;
+
 import com.bridgelabz.notes.model.Label;
 
 public interface ILabelDao {
 	
-	public int addLabel(Label label, Long userId);
-	public int deleteLabel(Long noteId, Long labelId) ;
+	public int addLabel(Label label,Long noteId);
+	public boolean createLabel(Label label);
+	public int deleteLabel( Long labelId) ;
 	public int updateLable(Long labelId, String newName) ;
+	public boolean verifyUser(Long userId);
+	public Label getLableById(Long labelId);
+	public List<Label> getAllLabelByUserId(Long userId);
+
 }
