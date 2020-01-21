83c17a4ce296023a7086add1dd695dc6c7b545ce
Rupesh <patilrupesh990@gmail.com>
2020-01-21 18:53:27 +0530

    [Rupeshpatil].updated ILableService interface

--- src/main/java/com/bridgelabz/notes/service/ILabelService.java
+++ src/main/java/com/bridgelabz/notes/service/ILabelService.java
@@ -7,13 +7,15 @@ import com.bridgelabz.notes.response.LabelResponce;
 
 public interface ILabelService {
 	
-	public ResponseEntity<LabelResponce> addLable(String token, Label label) ;
+	public ResponseEntity<LabelResponce> createLable(String token, Label label); 
+	public ResponseEntity<LabelResponce> addLabel(String token,Long labelId,Long noteId);
 	
-	public ResponseEntity<LabelResponce> deleteLabel(String token, Long labelId, Long noteId); 
+	public ResponseEntity<LabelResponce> deleteLabel(String token, Long labelId); 
 	
 	public ResponseEntity<LabelResponce> editLabel(Long labelId,String newName,String token);
 	
 	public boolean verifyToken(String token);
+	public ResponseEntity<LabelResponce> getAllLabels(String token); 
 
 	//public void editLabel();
 	
