635424370b3b86ee9fce6210b15f398ed8de32a9
Rupesh <patilrupesh990@gmail.com>
2020-01-22 19:25:36 +0530

    [Rupesh-patil].Updated LabelService

--- src/main/java/com/bridgelabz/notes/service/LabelService.java
+++ src/main/java/com/bridgelabz/notes/service/LabelService.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.bridgelabz.notes.dao.ILabelDao;
+import com.bridgelabz.notes.dao.INotesDao;
 import com.bridgelabz.notes.model.Label;
+import com.bridgelabz.notes.model.Note;
 import com.bridgelabz.notes.response.LabelResponce;
 import com.bridgelabz.notes.util.JwtTokenUtil;
 
@@ -16,54 +19,94 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-public class LabelService implements ILabelService{
+public class LabelService implements ILabelService {
 
 	@Autowired
 	JwtTokenUtil tokenUtil;
 	@Autowired
 	ILabelDao labelDao;
-	String response400="Not Available or Token Expired";
+	@Autowired
+	INotesDao noteDao;
+	String response400 = "Not Available or Token Expired";
 
 	@Override
-	@Transactional
-	public ResponseEntity<LabelResponce> addLable(String token, Label label) {
-
-		
-		if (labelDao.addLabel(label, tokenUtil.parseToken(token)) > 0 && verifyToken(token))
+	public ResponseEntity<LabelResponce> createLable(String token, Label label) {
+		log.info("hdjifhdjifhdjkfhdjkfhd:----------------------------");
+		label.setUserId(tokenUtil.parseToken(token));
+		if (verifyToken(token) && labelDao.createLabel(label))
 			return ResponseEntity.status(HttpStatus.CREATED)
-					.body(new LabelResponce(201, "" + label.getLabelName() + " Label Added to ->" + label.getNoteId()));
+					.body(new LabelResponce(201, "" + label.getLabelName() + " Label Created Successfully "));
 		else
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-					.body(new LabelResponce(400, response400));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new LabelResponce(400, response400));
 	}
+
 	@Override
 	@Transactional
-	public ResponseEntity<LabelResponce> deleteLabel(String token, Long labelId, Long noteId) {
-		if (labelDao.deleteLabel(noteId, labelId) > 0 && verifyToken(token))
+	public ResponseEntity<LabelResponce> deleteLabel(String token, Long labelId) {
+		if (labelDao.deleteLabel(labelId) > 0 && verifyToken(token))
 			return ResponseEntity.status(HttpStatus.ACCEPTED)
 					.body(new LabelResponce(202, "label deleted Successfully"));
 		else
 			return ResponseEntity.status(HttpStatus.CONFLICT).body(new LabelResponce(400, response400));
 	}
+
 	@Override
 	@Transactional
-	public ResponseEntity<LabelResponce> editLabel(Long labelId,String newName,String token)
-	{
-		if(labelDao.updateLable(labelId, newName)>0 && verifyToken(token))
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new LabelResponce(201,"Label Updated"));
+	public ResponseEntity<LabelResponce> editLabel(Long labelId, String newName, String token) {
+		if (labelDao.updateLable(labelId, newName) > 0 && verifyToken(token))
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new LabelResponce(201, "Label Updated"));
 		else
 			return ResponseEntity.status(HttpStatus.CONFLICT).body(new LabelResponce(400, response400));
-			
 	}
+
+	@Transactional
+	// input note id,label id
+	public ResponseEntity<LabelResponce> addLabel(String token, Long labelId, Long noteId) {
+
+		log.error("Msg"+noteId);
+		Note note = noteDao.findNoteById(noteId);
+		Label labelObject = labelDao.getLableById(labelId);
+		log.info("LAbel NAme---------"+labelObject.getLabelName());
+		log.info("Label Id-----------"+labelObject.getLabelId());
+		log.info("Label NoteId-----------"+labelObject.getNoteId());
+		note.getLabelList().add(labelObject);
+
+		if (note != null && verifyToken(token) && labelObject != null) {
+
+			if (labelDao.addLabel(labelObject, noteId) > 0) {
+				return ResponseEntity.status(HttpStatus.ACCEPTED).body(new LabelResponce(201,
+						labelObject.getLabelName() + "Label AddedSuccessfully to->" + labelObject.getNoteId()));
+			} else {
+				return ResponseEntity.status(HttpStatus.CONFLICT).body(new LabelResponce(400, response400));
+			}
+		}
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(new LabelResponce(400, response400));
+	}
+
+	public ResponseEntity<LabelResponce> getAllLabels(String token) {
+		if (labelDao.verifyUser(tokenUtil.parseToken(token))) {
+			Long userId = tokenUtil.parseToken(token);
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new LabelResponce(201, labelDao.getAllLabelByUserId(userId),
+					"Number Of Labels Are: " + labelDao.getAllLabelByUserId(userId).size()));
+		} else {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(new LabelResponce(400, response400));
+		}
+	}
+
+	
 	@Override
-	public boolean verifyToken(String token)
-	{
+	public boolean verifyToken(String token) {
 		try {
-			tokenUtil.parseToken(token);
-			return true;
+			log.info("incoming value first check:" + tokenUtil.parseToken(token));
+			if (labelDao.verifyUser(tokenUtil.parseToken(token))) {
+				return true;
+
+			} else {
+				return false;
+			}
 		} catch (Exception e) {
 			return false;
-		}	
+		}
 	}
 
 }
