9acbe7e97f051b58b4587820f30c15cd5dfa414f
Rupesh <patilrupesh990@gmail.com>
2020-01-31 19:28:33 +0530

    [Rupesh-patil].upadetd LabelController

--- src/main/java/com/bridgelabz/notes/controller/LabelController.java
+++ src/main/java/com/bridgelabz/notes/controller/LabelController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.notes.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.http.ResponseEntity;
@@ -16,40 +18,46 @@ import com.bridgelabz.notes.model.Label;
 import com.bridgelabz.notes.response.LabelResponce;
 import com.bridgelabz.notes.service.ILabelService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @EnableEurekaClient
 @RestController
 @RequestMapping("/labels")
+@Slf4j
 public class LabelController {
-	
+
 	@Autowired
 	ILabelService labelService;
-	
+
 	@PostMapping("/add")
-	public ResponseEntity<LabelResponce> addLable(@RequestHeader String token,@RequestHeader Long labelId,@RequestHeader Long noteId)
-	{
-		return labelService.addLabel(token, labelId,noteId);
+	public ResponseEntity<Object> addLable(@RequestHeader String token, @RequestHeader Long labelId,
+			@RequestHeader Long noteId) {
+		return labelService.addLabel(token, labelId, noteId);
 	}
+
 	@PostMapping("/create")
-	public ResponseEntity<LabelResponce> createLabel(@RequestHeader String token,@RequestBody Label label)
-	{
+	public ResponseEntity<Object> createLabel(@RequestHeader String token, @RequestBody Label label) {
+		log.info("Create Label Controller");
 		return labelService.createLable(token, label);
 	}
+
 	@DeleteMapping("/delete")
-	public  ResponseEntity<LabelResponce> deleteLabel(@RequestHeader String token,@RequestHeader Long labelId)
-	{
+	public ResponseEntity<Object> deleteLabel(@RequestHeader String token, @RequestHeader Long labelId) {
 		return labelService.deleteLabel(token, labelId);
 	}
+
 	@PutMapping("/edit")
-	public  ResponseEntity<LabelResponce> editLabel(@RequestHeader Long labelId,@RequestHeader String newName,@RequestHeader String token)
-	{
+	public ResponseEntity<Object> editLabel(@RequestHeader Long labelId, @RequestHeader String newName,
+			@RequestHeader String token) {
 		return labelService.editLabel(labelId, newName, token);
 	}
-	
+
 	@GetMapping("/label")
-	public ResponseEntity<LabelResponce> getAllLabels(@RequestHeader String token)
-	{
+	public ResponseEntity<Object> getAllLabels(@RequestHeader String token) {
 		return labelService.getAllLabels(token);
 	}
-	
-	
+	@GetMapping("/notes")
+	public List<Object[]> getNotesByLabelId(@RequestHeader String token,@RequestHeader Long labelId) {
+		return labelService.getNotesByLabelId(token, labelId);
+	}
 }
